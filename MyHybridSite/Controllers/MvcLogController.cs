7491adb96cc3564426649bc21387232b91fe1806
Daniel Engberg <daniel@engbergit.dk>
2015-05-19 16:16:25 +0200

    Example MVC site from dev camp.

---
+++ MyHybridSite/Controllers/MvcLogController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyHybridSite.Models;
+
+namespace MyHybridSite.Controllers
+{
+    public class MvcLogController : Controller
+    {
+        // GET: MvcLog
+        public async Task<ActionResult> Index()
+        {
+            Guid logId = Guid.NewGuid();
+            Log log = new Log();
+            log.Id = logId;
+            log.TimeStamp = DateTime.Now;
+            log.Message = "Message 1";
+            Log.LogList.Add(logId, log);
+            logId = Guid.NewGuid();
+            log = new Log();
+            log.Id = logId;
+            log.TimeStamp = DateTime.Now;
+            log.Message = "Message 2";
+            Log.LogList.Add(logId, log);
+
+            return View("Index", Log.LogList.Values);
+        }
+
+        // GET: MvcPerson/Edit/5
+        public async Task<ActionResult> Edit(Guid id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Log log;
+            if (!Log.LogList.TryGetValue(id, out log))
+                return HttpNotFound();
+
+            return View("Edit", log);
+        }
+    }
+}
---
+++ MyHybridSite/Models/Log.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyHybridSite.Models
+{
+    public class Log
+    {
+        public static Dictionary<Guid, Log> LogList = new Dictionary<Guid, Log>();
+
+        public Guid Id { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public string Message { get; set; }
+    }
+}
